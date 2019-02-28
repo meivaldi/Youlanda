b4a1bf517a7a008cae2265a51079d2552893a960
andreepratama27 <andreepratama27@gmail.com>
2019-02-28 19:17:27 +0700

    Done

--- app/src/main/java/com/meivaldi/youlanda/ui/MainActivity.java
+++ app/src/main/java/com/meivaldi/youlanda/ui/MainActivity.java
@@ -1,16 +1,22 @@
 package com.meivaldi.youlanda.ui;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.meivaldi.youlanda.R;
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private MainActivityViewModel viewModel;
     private RecyclerView recyclerView;
+    private ProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +52,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         MainViewModelFactory factory = InjectorUtils.provideMainActivityViewModelFactory(getApplicationContext());
         viewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel.class);
 
-        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.product_list);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 4);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(4, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         viewModel.getProductList().observe(this, products -> {
-            Toast.makeText(this, "" + products.size(), Toast.LENGTH_SHORT).show();
+            adapter = new ProductAdapter(getApplicationContext(), products);
+            recyclerView.setAdapter(adapter);
+            Toast.makeText(getApplicationContext(), "" + products.size(), Toast.LENGTH_SHORT).show();
         });
     }
 
@@ -76,5 +89,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view);
+            int column = position % spanCount;
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount;
+                outRect.right = (column + 1) * spacing / spanCount;
+
+                if (position < spanCount) {
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing;
+            } else {
+                outRect.left = column * spacing / spanCount;
+                outRect.right = spacing - (column + 1) * spacing / spanCount;
+                if (position >= spanCount) {
+                    outRect.top = spacing;
+                }
+            }
+        }
+    }
+
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
---
+++ app/src/main/java/com/meivaldi/youlanda/ui/ProductAdapter.java
@@ -0,0 +1,85 @@
+package com.meivaldi.youlanda.ui;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.meivaldi.youlanda.R;
+import com.meivaldi.youlanda.data.database.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
+
+    private List<Product> productList;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView thumbnail;
+        private TextView nama, harga;
+
+        public MyViewHolder(View view) {
+            super(view);
+            thumbnail = view.findViewById(R.id.thumbnail);
+            nama = view.findViewById(R.id.nama_produk);
+            harga = view.findViewById(R.id.harga_produk);
+        }
+
+    }
+
+    public ProductAdapter(Context context, List<Product> productList) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        }
+
+        View view = layoutInflater.inflate(R.layout.product_card, viewGroup, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, final int i) {
+        final Product product = productList.get(i);
+        myViewHolder.nama.setText(product.getNama());
+        myViewHolder.harga.setText("Rp." + product.getHarga());
+        myViewHolder.thumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Glide.with(context)
+                .load(product.getFoto())
+                .apply(new RequestOptions().transform(new RoundedCorners(30)))
+                .into(myViewHolder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public interface ProductAdapterListener {
+        void onProductClicked(Product product);
+    }
+
+}
