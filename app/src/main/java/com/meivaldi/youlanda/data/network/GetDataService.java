cccefa1b0f1d47aebf458f0b2ea996145b5eaab4
andreepratama27 <andreepratama27@gmail.com>
2019-02-28 12:01:31 +0700

    Pause

--- app/src/main/java/com/meivaldi/youlanda/data/network/GetDataService.java
+++ app/src/main/java/com/meivaldi/youlanda/data/network/GetDataService.java
@@ -5,11 +5,12 @@ import com.meivaldi.youlanda.data.database.Product;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 
 public interface GetDataService {
 
-    @GET("android/get_product.php")
-    Call<List<Product>> getAllProducts();
+    @POST("/android/get_product.php")
+    Call<List<Product>> getAllProducts(@Body String jenis);
 
 }
