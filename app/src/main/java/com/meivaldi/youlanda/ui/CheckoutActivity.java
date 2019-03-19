67b1a96933d9ef87c2bf26e7ed0345dada2bbcb2
andreepratama27 <andreepratama27@gmail.com>
2019-03-19 16:22:37 +0700

    Done

---
+++ app/src/main/java/com/meivaldi/youlanda/ui/CheckoutActivity.java
@@ -0,0 +1,15 @@
+package com.meivaldi.youlanda.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.meivaldi.youlanda.R;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
